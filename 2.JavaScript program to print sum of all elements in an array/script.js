9cbb9f1bb06ca766757b101ec28579989296ac03
Bipin Adhikari <adhikaribipin1032@gmail.com>
2023-03-31 05:38:29 +0545

    JavaScript program to find area of rectangle

--- 2.JavaScript program to print sum of all elements in an array/script.js
+++ 2.JavaScript program to print sum of all elements in an array/script.js
@@ -1,11 +1,87 @@
+// function arraySum(array) {
+//   let sum = 0;
+//   for (let i = 0; i < array.length; i++) {
+//     sum = sum + array[i];
+//   }
+//   return sum;
+// }
+// let arayNum = [2, 4];
+// const sum = arraySum(arayNum);
+
+// console.log(`The sum of array is ${sum}`);
+
+// function arraySum(array) {
+//   let sum = 0;
+//   for (let i = 0; i < array.length; i++) {
+//     sum = sum + array[i];
+//   }
+//   return sum;
+// }
+
+// let arrayNum = [5, 4];
+// const sum = arraySum(arrayNum);
+// console.log(`The sum of array is ${sum}`);
+
+// function arraySum(array) {
+//   let sum = 0;
+//   for (let i = 0; i < array.length; i++) {
+//     sum = sum + array[i];
+//   }
+//   return sum;
+// }
+// let arrayNum = [2, 5];
+// const sum = arraySum(arrayNum);
+// console.log(`The sum of array is ${sum}`);
+
+// function arraySum(array) {
+//   let sum = 0;
+//   for (let i = 0; i < array.length; i++) {
+//     sum = sum + array[i];
+//   }
+//   return sum;
+// }
+// let arrayNum = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const sum = arraySum(arrayNum);
+// console.log(`The sum of an array is ${sum}`);
+
+// function arraySum(array) {
+//   let sum = 0;
+//   for (let i = 0; i < array.length; i++) {
+//     sum = sum + array[i];
+//   }
+//   return sum;
+// }
+// let arrayNum = [2, 5, 8, 9];
+// const sum = arraySum(arrayNum);
+// console.log(`The sum of an array is ${sum}`);
+
 function arraySum(array) {
   let sum = 0;
   for (let i = 0; i < array.length; i++) {
     sum = sum + array[i];
   }
+  console.log("Hello World");
   return sum;
 }
-let arayNum = [2, 4];
-const sum = arraySum(arayNum);
 
-console.log(`The sum of array is ${sum}`);
+const sum = arraySum([2, 6]); //8
+console.log(`The sum of an array is ${sum}`); //The sum of an array is 8
+
+function addSum(a, b) {
+  let sum = a + b;
+  return sum;
+}
+const addedValue = addSum(2, 3); //5
+console.log(addedValue); //Print 5
+
+const arraySum2 = arraySum([1, 6, 7]);
+const a = 2;
+const b = 4;
+const sum2 = addSum(a, b);
+console.log(arraySum2, sum2);
+
+// function arraySum(array)
+// {
+//   let sum=0;
+//   for(let i=0;i<array.length;i++)
+// }
---
+++ 3.JavaScript program to find area of rectangle/script.js
@@ -0,0 +1,4 @@
+let length = 5;
+let width = 4;
+let area = length * width;
+console.log("The area of rectangle is", area);
