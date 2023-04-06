cae40dddf020663c175a20d6dbc8435c73fd5259
Bipin Adhikari <adhikaribipin1032@gmail.com>
2023-04-06 05:49:10 +0545

    JavaScript program to find odd or even number using if else

--- 7.JavaScript program to add two number using function/script.js
+++ 7.JavaScript program to add two number using function/script.js
@@ -1,5 +1,77 @@
-function addNum(Num1, Num2) {
-  return Num1 + Num2;
-}
-let result = addNum(4, 5);
-console.log(result);
+// JavaScript program to print sum of all element in an array
+
+// function arraySum(arr) {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     sum = sum + arr[i];
+//   }
+//   return sum;
+// }
+
+// let array = [1, 2, 3, 5, 4];
+// const sum = arraySum(array);
+// console.log(`The sum of an array is ${sum}`);
+
+// const sum2 = arraySum([1, 2, 3]);
+// console.log(`The sum of an array is ${sum2}`);
+
+// console.log(`The sum of an array is ${arraySum([2, 6])}`);
+
+// JavaScript program to find the sum of even number given in an array
+
+// function arraySum(arr) {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 == 0) {
+//       sum = sum + arr[i];
+//     }
+//   }
+//   return sum;
+// }
+
+// let array = [1, 2, 3, 4, 5, 6];
+// const sum = arraySum(array);
+// console.log(`The sum of an array is ${sum}`);
+
+// const sum1 = arraySum([1, 2]);
+// console.log(`The sum of an array is ${sum1}`);
+
+// console.log(`The sum of an array is ${arraySum([2, 4, 5])}`);
+
+//Function to find the sum of all the positive number in an array
+
+// function arraySum(arr) {
+//   let sum = 0;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > 0) {
+//       sum = sum + arr[i];
+//     }
+//   }
+//   return sum;
+// }
+// let array = [-1, -2, 3, 4];
+// //sending the value
+// const sum = arraySum(array);
+
+// console.log(`The sum of an array is ${sum}`);
+
+//Function to find the sum of odd number in an array
+
+// function arraySum(arr) {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 !== 0) {
+//       sum = sum + arr[i];
+//     }
+//   }
+//   return sum;
+// }
+
+// let array = [1, 2, 3, 4, 5, 6];
+// //sending the value
+// const sum = arraySum(array);
+
+// console.log(`The sum of an array is ${sum}`);
+
+//JavaScript program to print sum of positive number  a given array
---
+++ 8.JavaScript program to find odd or even number using if else/script.js
@@ -0,0 +1,6 @@
+const number = prompt("Enter The number ");
+if (number % 2 == 0) {
+  console.log("The number is Even");
+} else {
+  console.log("The number is odd");
+}
