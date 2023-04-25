67cd4a260ef2c5c8dfdf5d19038f8aa63cc93ab4
Bipin Adhikari <adhikaribipin1032@gmail.com>
2023-04-25 17:23:21 +0545

    Jafunction reverseString(str) {

--- 25.JavaScript program to reverse a string function/script.js
+++ 25.JavaScript program to reverse a string function/script.js
@@ -4,10 +4,12 @@ function reverseString(str) {
   for (let i = str.length - 1; i >= 0; i--) {
     reversed += str[i];
   }
+
   return reversed;
 }
-let string = "Bipin";
 
-let reversed = reverseString(string);
+let strings = "Bipin Adhikari";
+
+let reversed = reverseString(strings);
 
 console.log(reversed);
