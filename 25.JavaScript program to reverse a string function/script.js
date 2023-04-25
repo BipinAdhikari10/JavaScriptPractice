f6e1b195578b7de6c739ba743aa657ddebf41c50
Bipin Adhikari <adhikaribipin1032@gmail.com>
2023-04-25 13:55:41 +0545

    Jafunction reverseString(str) {

--- 25.JavaScript program to reverse a string function/script.js
+++ 25.JavaScript program to reverse a string function/script.js
@@ -1,11 +1,14 @@
 function reverseString(str) {
   let reversed = "";
-  for (let i = str.length - 1; i >= 0; i--) {
+
+  for (i = str.length - 1; i >= 0; i--) {
     reversed += str[i];
   }
+
   return reversed;
 }
+let string = "apple";
 
-let originalString = "Hello, world!";
-let reversedString = reverseString(originalString);
-console.log(reversedString);
+let reversed = reverseString(string);
+
+console.log(reversed);
