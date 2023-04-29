81d80d8e74d9bd82f4450a9d28ebb8a3f49c9838
Bipin Adhikari <adhikaribipin1032@gmail.com>
2023-04-29 18:29:56 +0545

    Function that takes an array of numbers and returns the average of all numbers in the array

--- 29.Function that takes an array of numbers and returns the average of all numbers in the array/script.js
+++ 29.Function that takes an array of numbers and returns the average of all numbers in the array/script.js
@@ -3,8 +3,9 @@ function calculateAverage(arr) {
   for (let i = 0; i < arr.length; i++) {
     sum += arr[i];
   }
-  return sum;
+  return sum / arr.length;
 }
-const numbers = [2, 4, 6, 8, 10];
+
+const numbers = [1, 2, 3, 4, 5];
 const average = calculateAverage(numbers);
-console.log(average);
+console.log(`The average number is ${average}`);
