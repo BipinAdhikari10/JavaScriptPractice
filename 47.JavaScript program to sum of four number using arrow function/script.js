49fb194f7373ed225ef57fdced8135f674cdcf3c
Bipin Adhikari <adhikaribipin1032@gmail.com>
2023-05-16 08:35:49 +0545

    JavaScript program to sum of four number using an arrow function

---
+++ 47.JavaScript program to sum of four number using arrow function/script.js
@@ -0,0 +1,9 @@
+const sumOfFourNumbers = (num1, num2, num3, num4) => num1 + num2 + num3 + num4;
+
+const number1 = 2;
+const number2 = 5;
+const number3 = 7;
+const number4 = 3;
+
+const result = sumOfFourNumbers(number1, number2, number3, number4);
+console.log("The sum of the four numbers is:", result);
