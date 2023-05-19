bedb96f4a576819420477a9b240908de022b242b
Bipin Adhikari <adhikaribipin1032@gmail.com>
2023-05-19 10:36:23 +0545

    JavaScript program to sum of seven numbers using function

---
+++ 50.JavaScript program to sum of seven number using function/script.js
@@ -0,0 +1,7 @@
+function calculateSum(num1, num2, num3, num4, num5, num6, num7) {
+  return num1 + num2 + num3 + num4 + num5 + num6 + num7;
+}
+
+// Example usage
+var result = calculateSum(1, 2, 3, 4, 5, 6, 7);
+console.log(result); // Output: 28
