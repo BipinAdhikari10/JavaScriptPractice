052d5dfe061854bf75157e3582c336e1c4af77bd
Bipin Adhikari <adhikaribipin1032@gmail.com>
2023-05-20 20:23:02 +0545

    JavaScript program to sum of eight number using function

---
+++ 51.JavaScript program to sum of eight number using function/script.js
@@ -0,0 +1,7 @@
+function sumOfEightNumbers(num1, num2, num3, num4, num5, num6, num7, num8) {
+  return num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8;
+}
+
+// Example usage
+var result = sumOfEightNumbers(1, 2, 3, 4, 5, 6, 7, 8);
+console.log(result); // Output: 36
