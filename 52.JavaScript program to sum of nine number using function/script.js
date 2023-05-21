2b6026542574500d1b97eb5ba10f57f6edc8847e
Bipin Adhikari <adhikaribipin1032@gmail.com>
2023-05-21 20:07:59 +0545

    JavaScript program to sum of nine number using function

---
+++ 52.JavaScript program to sum of nine number using function/script.js
@@ -0,0 +1,17 @@
+function sumOfNineNumbers(
+  num1,
+  num2,
+  num3,
+  num4,
+  num5,
+  num6,
+  num7,
+  num8,
+  num9
+) {
+  return num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9;
+}
+
+// Example usage
+var result = sumOfNineNumbers(1, 2, 3, 4, 5, 6, 7, 8, 9);
+console.log(result); // Output: 45
