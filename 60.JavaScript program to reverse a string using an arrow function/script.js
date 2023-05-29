e1de18d32d2d7e67ac15bbaa5665b79088927fa4
Bipin Adhikari <adhikaribipin1032@gmail.com>
2023-05-29 19:46:12 +0545

    JavaScript program to reverse a string using an arrow function

---
+++ 60.JavaScript program to reverse a string using an arrow function/script.js
@@ -0,0 +1,6 @@
+const reverseString = (str) => str.split("").reverse().join("");
+
+// Example usage
+const originalString = "Hello, world!";
+const reversedString = reverseString(originalString);
+console.log(reversedString);
