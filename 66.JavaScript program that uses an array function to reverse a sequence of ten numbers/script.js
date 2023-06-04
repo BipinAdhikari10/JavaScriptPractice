d64426d1076ce72d110b5a2cc0403ac962a5d013
Bipin Adhikari <adhikaribipin1032@gmail.com>
2023-06-04 19:49:56 +0545

    JavaScript program that uses an array function to reverse a sequence of ten numbers

---
+++ 66.JavaScript program that uses an array function to reverse a sequence of ten numbers/script.js
@@ -0,0 +1,8 @@
+// Define the sequence of numbers
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// Use the array function `reverse` to reverse the sequence
+const reversedNumbers = [...numbers].reverse();
+
+// Print the reversed sequence
+console.log(reversedNumbers);
