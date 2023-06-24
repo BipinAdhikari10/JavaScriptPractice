bbead30e31a992edc150eb92c52a725f675bb6b0
Bipin Adhikari <adhikaribipin1032@gmail.com>
2023-06-24 19:53:47 +0545

    Check if all elements in an array are even

---
+++ 94.Check if all elements in an array are even/script.js
@@ -0,0 +1,14 @@
+function areAllEven(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 !== 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const numbers1 = [2, 4, 6, 8, 10];
+console.log(areAllEven(numbers1)); // Output: true
+
+const numbers2 = [2, 4, 6, 8, 11];
+console.log(areAllEven(numbers2)); // Output: false
