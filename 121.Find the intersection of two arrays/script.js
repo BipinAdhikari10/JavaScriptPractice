502c2a7be81c9bb1a63f6961e75769f460948caa
Bipin Adhikari <adhikaribipin1032@gmail.com>
2023-07-22 20:23:11 +0545

    Find the intersection of two arrays

---
+++ 121.Find the intersection of two arrays/script.js
@@ -0,0 +1,8 @@
+function findIntersection(arr1, arr2) {
+  const intersection = arr1.filter((element) => arr2.includes(element));
+  return intersection;
+}
+
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [3, 4, 5, 6, 7];
+console.log("Intersection: ", findIntersection(array1, array2));
