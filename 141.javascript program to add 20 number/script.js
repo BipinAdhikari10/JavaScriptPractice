35037b9c7f52f5ec0b9bbb7646ca7fbd01e33c86
Bipin Adhikari <adhikaribipin1032@gmail.com>
2023-08-17 19:53:28 +0545

    JavaScripy program to add 20 number

---
+++ 141.javascript program to add 20 number/script.js
@@ -0,0 +1,10 @@
+const numbers = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+];
+let sum = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+  sum += numbers[i];
+}
+
+console.log("Sum of the 20 numbers:", sum);
