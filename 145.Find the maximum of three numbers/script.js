86d0d2e547e673edc7e607c206ec539ccd6d0872
Bipin Adhikari <adhikaribipin1032@gmail.com>
2023-08-27 19:28:12 +0545

    Find the maximum of three numbers

---
+++ 145.Find the maximum of three numbers/script.js
@@ -0,0 +1,8 @@
+function findMaxOfThree(a, b, c) {
+  return Math.max(a, b, c);
+}
+
+const num1 = 8;
+const num2 = 5;
+const num3 = 12;
+console.log("Maximum number: " + findMaxOfThree(num1, num2, num3));
